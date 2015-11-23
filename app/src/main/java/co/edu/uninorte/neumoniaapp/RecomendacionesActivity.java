78417152dba665adc106d82b8fefc0621ec9f525
Daniel Padilla <danperzipa@hotmail.com>
2015-11-23 10:07:03 -0500

    .......

--- app/src/main/java/co/edu/uninorte/neumoniaapp/RecomendacionesActivity.java
+++ app/src/main/java/co/edu/uninorte/neumoniaapp/RecomendacionesActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -133,8 +134,9 @@ public class RecomendacionesActivity extends AppCompatActivity {
                     p1.setIdPaciente(idpaciente);
                     db.addPatient(p1);
                     Patient prueba = db.getPatient(idpaciente);
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    Intent intent = new Intent(context, MainActivity.class);
+                    Log.d("Desarrollo", "Se abrio el RiesgoSocial");
+                    startActivity(intent);
                 }
             }
         });
