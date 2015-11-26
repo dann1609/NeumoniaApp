a1a252ff471d58615413485d7bf97f78e1d9af61
augustosalazar@uninorte.edu.co <Be2l2000>
2015-11-25 19:36:48 -0500

    Ya funciona, pero hay que cambiarlo por entrada de datos correcta

--- app/src/main/java/co/edu/uninorte/neumoniaapp/ModificarDatosActivity.java
+++ app/src/main/java/co/edu/uninorte/neumoniaapp/ModificarDatosActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
@@ -20,6 +21,7 @@ public class ModificarDatosActivity extends AppCompatActivity {
     private CheckAdapter adapter;
     ArrayList values= new ArrayList<String>();
     public static ArrayList editValues=new ArrayList<Boolean>();
+    private String TAG = "testModificarDatos";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +34,11 @@ public class ModificarDatosActivity extends AppCompatActivity {
         values.addAll(Arrays.asList(getResources().getStringArray(R.array.comorbilidades)));
 
 
-        mLv=(ListView)findViewById(R.id.listView);
+        Log.d(TAG, "values  size " +values.size());
+        mLv=(ListView)findViewById(R.id.listViewModificarDatos);
         adapter=new CheckAdapter(this,values);
         mLv.setAdapter(adapter);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
